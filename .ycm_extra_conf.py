ac72b219a4b384c122eab39627db0b1f2a8a03db
Daniel Cole <dscole@users.noreply.github.com>
2016-11-18 12:45:15 -0500

    Use libc++ instead of c++/4.8 for includes

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -48,6 +48,7 @@ flags = [
     # For a C project, you would set this to something like 'c99' instead of
     # 'c++11'.
     '-std=c++14',
+    '-stdlib=libc++',
     # ...and the same thing goes for the magic -x option which specifies the
     # language that the files to be compiled are written in. This is mostly
     # relevant for c++ headers.
@@ -65,7 +66,7 @@ flags = [
     # '-isystem',
     # './tests/gmock/include',
     '-isystem', '/usr/include',
-    '-isystem', '/usr/include/c++/4.8',
+    # '-isystem', '/usr/include/c++/4.8',
     '-isystem', '/usr/include/x86_64-linux-gnu/c++/4.8',
     '-isystem', '/usr/include/x86_64-linux-gnu',
 ]
