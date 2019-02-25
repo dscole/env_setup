c9b3bb81bf6eba8e5d50f85c240c3d0f2536d019
Daniel Cole <sdaniel.cole@gmail.com>
2019-02-25 00:18:38 -0500

    Mount org folder

--- run_docker.sh
+++ run_docker.sh
@@ -7,10 +7,10 @@ docker run --rm \
            -e UHOME=$HOME \
            -v $HOME/.ssh:$HOME/.ssh \
            -v $HOME/workspace:$HOME \
+           -v $HOME/org:$HOME/org \
            -v /etc/localtime:/etc/localtime \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
            -v /var/run/docker.sock:/var/run/docker.sock \
            -v $HOME:/mnt/workspace \
-           -v ~/.diskstation.creds:/home/emacs/.diskstation.creds \
            --privileged \
            dscole/emacs
